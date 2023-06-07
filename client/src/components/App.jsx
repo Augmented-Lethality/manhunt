c570466cc94202492d555fba035936c8db632a14
logan <loganh42@gmail.com>
2023-06-07 18:57:46 -0500

    Add React and Rendered to Page

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+
+
+  return (
+    <h1>Manhunt in App.jsx</h1>
+  )
+};
+
+export default App
--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
 const port = process.env.PORT || 3666;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hi Man Hunters');
-});
+app.use(express.json()); // handles parsing content in the req.body from post/update requests
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+app.use(express.urlencoded({ extended: true })); // Parses url (allows arrays and objects)
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${ port }`);
-})
-
+  console.log(`Server listening on port ${port}`);
+});
