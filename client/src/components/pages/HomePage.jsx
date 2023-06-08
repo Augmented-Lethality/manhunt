6d2bb0ed17559c2a440af775bf2dba4e9a0cb832
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-08 18:42:35 -0500

    Add homepage

---
+++ client/src/components/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {LogoutButton} from '../Auth0/LogoutButton.jsx'
+
+const HomePage = () => {
+
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <LogoutButton/>
+    </div>
+  )
+};
+
+export default HomePage
