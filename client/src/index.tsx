fd624d84a6781a964161185b7dcfa1fdc4d60fbd
logan <loganh42@gmail.com>
2023-06-08 18:48:50 -0500

    Add .env File

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import App from './components/App';
 
 const rootElement = document.getElementById('app');
 
+// HTMLElement | null; had to add error handling or has an error
 if (!rootElement){
   throw new Error('Failed to find the root element');
 };
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import * as path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3666;
 
 app.use(express.json()); // handles parsing content in the req.body from post/update requests
-app.use(express.static(path.resolve(__dirname, '../client/dist')));
-app.use(express.urlencoded({ extended: true })); // Parses url (allows arrays and objects)
+app.use(express.static(path.resolve(__dirname, '../client/dist'))); // serves static file from client side in dist, created by webpack
+app.use(express.urlencoded({ extended: true })); // parses url (allows arrays and objects)
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
