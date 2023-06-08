2f3ddff6ca124b6be6b17a3138d1e7042f787402
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-08 18:35:39 -0500

    Add router and Auth0Provider import

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-
+import { Auth0ProviderWithNavigate } from "./components/Auth0/auth0-provider-with-navigate";
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
-// import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-<Auth0Provider
-    domain="dev-cxttpv5tsynl0jh2.us.auth0.com"
-    clientId="TJtAYxTTITDgtZVckBeEds6zN7OPMoiU"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
