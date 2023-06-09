f5aa1782d265eef0aedf62ce6e14320c987c0edf
logan <loganh42@gmail.com>
2023-06-09 13:22:29 -0500

    Add Socketio Initialization

---
+++ server/websocket/socketEvents.ts
@@ -0,0 +1,3 @@
+// i wouldn't crowd this with all of the different events!
+// if you're seeing this, you probably want to put a new event for socket. go check and/or make another file so we can keep
+// our work separate but still together. <3
