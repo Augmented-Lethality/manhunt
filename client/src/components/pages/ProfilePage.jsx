37d74d547b30ec63a5415ccdfe0f3532bcf5d2a0
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-08 19:34:05 -0500

    Add a bunch of demo info to profilePage

--- client/src/components/pages/ProfilePage.jsx
+++ client/src/components/pages/ProfilePage.jsx
@@ -1,22 +1,48 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
-const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+const ProfilePage = () => {
+  const { user } = useAuth0();
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
+  if (!user) {
+    return null;
   }
 
   return (
-    isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+    <div className="content-layout">
+      <h1 id="page-title" className="content__title">
+        Profile Page
+      </h1>
+      <div className="content__body">
+        <p id="page-description">
+          <span>
+            You can use the <strong>ID Token</strong> to get the profile
+            information of an authenticated user.
+          </span>
+          <span>
+            <strong>Only authenticated users can access this page.</strong>
+          </span>
+        </p>
+        <div className="profile-grid">
+          <div className="profile__header">
+            <img
+              src={user.picture}
+              alt="Profile"
+              className="profile__avatar"
+            />
+            <div className="profile__headline">
+              <h2 className="profile__title">{user.name}</h2>
+              <span className="profile__description">{user.email}</span>
+            </div>
+          </div>
+          <div className="profile__details">
+            <h2>Decoded ID Token</h2>
+            <p>{JSON.stringify(user, null, 2)}</p>
+          </div>
+        </div>
       </div>
-    )
+    </div>
   );
 };
 
-export default Profile;
+export default ProfilePage;
