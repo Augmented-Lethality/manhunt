03f9cd87928deab6a00ffe3d5fe49b9e8410c22e
Logan Hochwald <loganh42@gmail.com>
2023-06-10 14:48:46 -0500

    Revert "Added production config for webpack, typescript compilers, and package.json scripts"

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,11 @@ const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const srcDir = path.resolve(__dirname, "client/src");
-const distDir = path.resolve(__dirname, "dist/client");
-console.log(distDir)
+const distDir = path.resolve(__dirname, "client/dist");
 
 module.exports = {
   mode: "development",
-  watch: true,
+  // watch: true,
   devtool: "eval-source-map",
   entry: {
     app: path.resolve(srcDir, "index.tsx"),
@@ -15,7 +14,11 @@ module.exports = {
   output: {
     path: distDir,
     filename: "bundle.js",
+    // publicPath: '/'
   },
+  // devServer: {
+  //   historyApiFallback: true,
+  // },
   plugins: [
     new HtmlWebPackPlugin({
       template: path.resolve(srcDir, "index.html"),
