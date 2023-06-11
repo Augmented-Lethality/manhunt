e3bf41932b3940964c2eb310527b8a9fbbd25c8c
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-11 17:45:05 -0500

    init

---
+++ server/database/seed.ts
@@ -0,0 +1,31 @@
+const { Client } =require('pg');
+
+// Create a new client instance
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  password: '',
+  port: 5432,
+});
+
+async function setupDatabase() {
+  try {
+    await client.connect();
+
+    // Drop the database if it exists
+    await client.query('DROP DATABASE IF EXISTS manhunt');
+
+    // Create the database
+    await client.query('CREATE DATABASE manhunt');
+
+    console.log('Database setup complete.');
+  } catch (error) {
+    console.error('Error setting up database:', error);
+  } finally {
+    // Make sure to always close the client
+    await client.end();
+  }
+}
+
+setupDatabase();
+
