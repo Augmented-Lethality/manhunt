bd56669b57ce5d856fb156e6e255776ffea728cd
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-11 17:43:54 -0500

    Modify config to establish db connection

--- server/database/index.ts
+++ server/database/index.ts
@@ -1,11 +1,11 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-// Initialize DB
-const db = new Sequelize("Manhunt", "root", "", {
-  host: "localhost", // `host` parameter required for other databases
-  dialect: "mysql",
-  logging: false, // Avoids printing all syncing messages in server 
-  
+// parameters are db, user, and password
+const db = new Sequelize('manhunt', 'postgres', '', {
+  host: 'localhost',
+  port: 5432,
+  dialect: 'postgres',
+  logging: false, // Avoids printing all syncing messages in server
 });
 
 // Sequelize Authenticate
@@ -14,7 +14,6 @@ db.sync({alter: true})  // Syncs the database and models
     console.log("Tables synched.");
     db.authenticate()
   })
-
   .then(() => {
     console.log("Connected to database.");
   })
@@ -22,10 +21,6 @@ db.sync({alter: true})  // Syncs the database and models
     console.error("Unable to connect to database:", err);
   });
 
-db.query("set foreign_key_checks = 0");
+  db.query("set foreign_key_checks = 0");
 
-
-export {
-  db,
-};
-
+  module.exports = { db };
