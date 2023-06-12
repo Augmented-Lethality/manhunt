4f3bdb97879dbd5d50a958c4cd071c14e358d41d
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-12 15:19:40 -0500

    remove console.log

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -2,8 +2,6 @@ const { Router } = require('express');
 const Users = Router();
 const { User } = require('../database/models')
 
-console.log('test3')
-
 Users.get('/', (req, res) => {
   try {
     res.status(200).send('some data')
