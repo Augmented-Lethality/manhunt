b6dd0403f6a338d9ac73e4e028b4609823f4ed2d
Alex <xanderlambert@gmail.com>
2023-06-14 19:05:21 -0500

    add PUT route to update user stats

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const Users = Router();
-const { User } = require('../database/models')
+const { User } = require("../database/models");
 
 // GET ALL USERS
-Users.get('/', async (req, res) => {
+Users.get("/", async (req, res) => {
   try {
     // Fetch all users from the database
     const users = await User.findAll();
@@ -11,40 +11,48 @@ Users.get('/', async (req, res) => {
     res.status(200).json(users);
   } catch (err) {
     console.warn(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-
 // GET SPECIFIC USER
-Users.get('/:authId', async (req, res) => {
+Users.get("/:authId", async (req, res) => {
   try {
     const authId = req.params.authId; // Assuming you have access to the authenticated user's ID
-    console.log('authId:', authId);
-    console.log('req.params.authId:', req.params.authId);
-    
+    console.log("authId:", authId);
+    console.log("req.params.authId:", req.params.authId);
+
     // Fetch the user's data from the database based on their google auth ID
     const user = await User.findOne({ where: { authId: req.params.authId } });
-    
-    console.log('User:', user);
+
+    console.log("User:", user);
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     res.status(200).json(user);
   } catch (err) {
     console.warn(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-
-
-// POST NEW USER, checks if user exists first
-Users.post('/', async (req, res) => {
+// POST NEW USER, checks if user exists first, returns existing if so
+Users.post("/", async (req, res) => {
   try {
-    const { username, email, authId, sessionId, gameId, location, tfModelPath, gamesPlayed, gamesWon, killsConfirmed } = req.body;
+    const {
+      username,
+      email,
+      authId,
+      sessionId,
+      gameId,
+      location,
+      tfModelPath,
+      gamesPlayed,
+      gamesWon,
+      killsConfirmed,
+    } = req.body;
 
     // Check if the user already exists
     const existingUser = await User.findOne({ where: { authId } });
@@ -53,7 +61,7 @@ Users.post('/', async (req, res) => {
       // User already exists, return the existing user in the response
       res.status(200).json(existingUser);
     } else {
-      // Create a new user in the database
+      // Else Create a new user in the database, return that new user.
       const newUser = await User.create({
         username,
         email,
@@ -70,9 +78,42 @@ Users.post('/', async (req, res) => {
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+
+// PUT updated stats into user
+Users.put("/:authId", async (req, res) => {
+  try {
+    const authId = req.params.authId;
+    const { gamesPlayed, gamesWon, killsConfirmed } = req.body;
+
+    // Find the user with the specified authId
+    const user = await User.findOne({ where: { authId } });
+
+    if (!user) {
+      // If no user was found with the specified authId
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Update the user's data in the database
+    user.gamesPlayed = gamesPlayed;
+    user.gamesWon = gamesWon;
+    user.killsConfirmed = killsConfirmed;
+
+    await user.save(); // Save the updated user object
+
+    // Return the updated user object in the response
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+
 
 
 module.exports = { Users };
