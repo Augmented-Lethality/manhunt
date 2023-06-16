175c907d173cc06deec1075d886e2f541751e792
Alex <xanderlambert@gmail.com>
2023-06-16 17:26:15 -0500

    add types for and display specific userData

--- client/src/components/pages/ProfilePage.tsx
+++ client/src/components/pages/ProfilePage.tsx
@@ -1,15 +1,18 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect, useState } from "react";
+import { useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { ButtonToHome } from "../Buttons";
-
+import { ButtonToHome } from '../Buttons';
 
 type UserData = {
   username: string;
   email: string;
   authId: string;
+  gamesPlayed: number;
+  gamesWon: number;
+  killsConfirmed: number;
   // Add other user data properties as needed
 }
+
 
 const ProfilePage: React.FC<{ userData: UserData | null }> = () => {
   const { user, isAuthenticated } = useAuth0();
@@ -19,7 +22,7 @@ const ProfilePage: React.FC<{ userData: UserData | null }> = () => {
     const fetchUserData = async () => {
       try {
         // Check if the user exists by sending a POST request instead of a GET request
-        const response = await axios.post<UserData>("/Users", {
+        const response = await axios.post<UserData>('/Users', {
           username: user?.name,
           email: user?.email,
           authId: user?.sub,
@@ -28,7 +31,7 @@ const ProfilePage: React.FC<{ userData: UserData | null }> = () => {
         setUserData(response.data);
         // console.log(response);
       } catch (error) {
-        console.error("Error fetching user data:", error);
+        console.error('Error fetching user data:', error);
       }
     };
 
@@ -36,39 +39,38 @@ const ProfilePage: React.FC<{ userData: UserData | null }> = () => {
       fetchUserData();
     }
   }, []);
-  console.log(userData, "USeRdatA");
-
-  
+  console.log(userData, 'USeRdatA');
 
   if (!user) {
     return null;
   }
 
   return (
-    <div className="content-layout">
-      <h1 id="page-title" className="content__title">
+    <div className='content-layout'>
+      <h1 id='page-title' className='content__title'>
         Profile
       </h1>
       <ButtonToHome />
-      <div className="content__body">
-        <p id="page-description">
-          <span>
-          </span>
+      <div className='content__body'>
+        <p id='page-description'>
+          <span></span>
           <span>
             <strong></strong>
           </span>
         </p>
-        <div className="profile-grid">
-          <div className="profile__header">
-            <img src={user.picture} alt="Profile" className="profile__avatar" />
-            <div className="profile__headline">
-              <h2 className="profile__title">{user.name}</h2>
-              <span className="profile__description">{user.email}</span>
+        <div className='profile-grid'>
+          <div className='profile__header'>
+            <img src={user.picture} alt='Profile' className='profile__avatar' />
+            <div className='profile__headline'>
+              <h2 className='profile__title'>{user.name}</h2>
+              <span className='profile__description'>{user.email}</span>
             </div>
           </div>
-          <div className="profile__details">
-            <h2>Decoded ID Token from authenticated user</h2>
-            <p>{JSON.stringify(user, null, 2)}</p>
+          <div className='profile__details'>
+            <h2>Games Played: {userData?.gamesPlayed}</h2>
+            <h2>Games Won: {userData?.gamesWon}</h2>
+            <h2>Kills Confirmed: {userData?.killsConfirmed}</h2>
+            {/* <h2>Suckers Ducked: {JSON.stringify(userData, null, 2)}</h2> */}
             <h2>userData from database</h2>
             <p>{JSON.stringify(userData, null, 2)}</p>
           </div>
