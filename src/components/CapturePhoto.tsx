915ffbf53298612db2d71514254b23557c081748
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-18 07:09:23 -0500

    Change Image to be in parent and ref to be webcamConext

--- src/components/CapturePhoto.tsx
+++ src/components/CapturePhoto.tsx
@@ -1,30 +1,32 @@
-import React, { useRef, useState, useCallback } from 'react';
-import Webcam from "react-webcam";
 
-const videoConstraints = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-  facingMode: "user"
-};
+import { useWebcam } from '../contexts/WebcamProvider';
+import React, { useState, useCallback } from 'react';
 
-const CapturePhoto: React.FC = () => {
-  const webcamRef = useRef<Webcam>(null);
-  const [imgSrc, setImgSrc] = useState<string | null>(null);
+interface CapturePhotoProps {
+  img: string | null;
+  setImg: (img: string | null) => void;
+}
+
+
+const CapturePhoto: React.FC<CapturePhotoProps> = ({img, setImg}) => {
+  const webcamContext = useWebcam();
+  const webcamRef = webcamContext?.webcamRef;
+  
 
   const capture = useCallback(() => {
-    const imageSrc = webcamRef.current?.getScreenshot();
+    const imageSrc = webcamRef?.current?.getScreenshot();
     if(imageSrc) { 
-      setImgSrc(imageSrc);
+      setImg(imageSrc);
     }
-  }, [webcamRef, setImgSrc]);
+  }, [webcamRef, setImg]);
 
   return (
     <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
-      {imgSrc ? (
+      {img ? (
         <>
-          <img src={imgSrc} alt="Screenshot" style={{ height: '100%', width: '100%', objectFit: 'cover' }} />
+          <img src={img} alt="Screenshot" style={{ height: '100%', width: '100%', objectFit: 'cover' }} />
           <button
-            onClick={()=>setImgSrc(null)}
+            onClick={()=>setImg(null)}
             style={{
               position: 'absolute',
               top: '90%',
@@ -38,12 +40,9 @@ const CapturePhoto: React.FC = () => {
               width: '100px',
               height: '50px',
             }}
-          >
-          RETAKE
-          </button>
-
+          >RETAKE</button>
           <button
-            onClick={()=>setImgSrc(null)}
+            onClick={()=>setImg(null)}
             style={{
               position: 'absolute',
               top: '90%',
@@ -57,42 +56,27 @@ const CapturePhoto: React.FC = () => {
               width: '100px',
               height: '50px',
             }}
-          >
-          SAVE
-          </button>
+          >SAVE</button>
         </>
         
       ) : (
-        <>
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            height={window.innerHeight}
-            screenshotFormat="image/jpeg"
-            width={window.innerWidth}
-            videoConstraints={videoConstraints}
-            style={{ position: 'absolute', top: 0, left: 0 }}
-          />
-          <button
-            onClick={capture}
-            style={{
-              position: 'absolute',
-              top: '80%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              background: 'rgba(255, 255, 255, 0.4)',
-              borderRadius: '50%',
-              boxShadow: 'rgba(255, 255, 255, 0.5) 0px 0px 100px',
-              border: '2px solid white',
-              backdropFilter: 'blur(5px)',
-              width: '100px',
-              height: '100px',
-            }}
-          />
-        </>
-
+        <button
+          onClick={capture}
+          style={{
+            position: 'absolute',
+            top: '80%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            background: 'rgba(255, 255, 255, 0.4)',
+            borderRadius: '50%',
+            boxShadow: 'rgba(255, 255, 255, 0.5) 0px 0px 100px',
+            border: '2px solid white',
+            backdropFilter: 'blur(5px)',
+            width: '100px',
+            height: '100px',
+          }}
+        />
       )}
-
     </div>
   );
 };
