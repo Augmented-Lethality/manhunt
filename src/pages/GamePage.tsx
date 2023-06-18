1dbcc886150ea94c04ef2f76fffd352c2d926a3a
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-18 16:34:25 -0500

    Add faceMatcher loading

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,27 +1,59 @@
 import React, { useState, useContext, useEffect } from 'react';
 import SocketContext from '../contexts/Socket/SocketContext';
 import { WebcamProvider } from '../contexts/WebcamProvider'
+import * as faceapi from 'face-api.js';
 // import KillMode from '../components/KillMode';
 import ChaseCam from '../components/ChaseCam';
 import KillCam from '../components/KillCam';
 
 import { ButtonToHome } from '../components/Buttons';
 
-
 const GamePage: React.FC = () => {
 
   // which component do we render? kill or chase?
   const [gameMode, setGameMode] = useState<string>('Chase');
-
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [faceMatcher, setFaceMatcher] = useState<faceapi.FaceMatcher | null>(null);
   const { uid, games, names } = useContext(SocketContext).SocketState;
-
   const [currentGame, setUserGame] = useState<{ gameId: string; uidList: string[], hunted: string }>({ gameId: '', uidList: [], hunted: '' });
 
   useEffect(() => {
     const foundUserGame = Object.values(games).find((game) => game.uidList.includes(uid));
     setUserGame(foundUserGame || { gameId: '', uidList: [], hunted: '' });
-
   }, [uid]);
+
+  useEffect(() => {
+    loadModels();
+    console.log('models loaded')
+  }, []);
+
+  const loadModels = async () => {
+    try {
+      await faceapi.loadSsdMobilenetv1Model('/models')
+      await faceapi.loadFaceLandmarkModel('/models')
+      await faceapi.loadFaceRecognitionModel('/models')
+      setModelsLoaded(true);
+      createFaceMatcher();
+    } catch (err) {
+      console.error(err);
+      setModelsLoaded(false);
+    }
+  };
+
+  const createFaceMatcher = async () => {
+    const labels = ['kalypso-homan'];
+    const promises = labels.map(async label => {
+      const descriptions: Float32Array[] = [];
+      const img = await faceapi.fetchImage(`assets/${label}.jpg`);
+      const detection = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+      if(detection){
+        descriptions.push(detection.descriptor);
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    });
+    const labeledFaceDescriptors = await Promise.all(promises);
+    setFaceMatcher( new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6));
+  }
 
   const handleGameChange = () => {
     if(gameMode === 'Chase') {
