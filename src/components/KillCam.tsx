b47e0035f343279c4671bf6002b6ca1ac20feb14
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-18 20:03:47 -0500

    Remove unused functions and states

--- src/components/KillCam.tsx
+++ src/components/KillCam.tsx
@@ -1,51 +1,49 @@
 import * as faceapi from 'face-api.js';
 import React, {useEffect, useState, useRef} from 'react';
+import { useWebcam } from '../contexts/WebcamProvider';
+import { useNavigate } from 'react-router-dom';
 
 type KillCamProps = {
   faceMatcher: (faceapi.FaceMatcher | null)
 }
 const KillCam: React.FC<KillCamProps> = ({faceMatcher}) => {
-  const [captureVideo, setCaptureVideo] = useState(false);
-
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const webcamContext = useWebcam();
+  const webcamRef = webcamContext?.webcamRef;
+  const  videoStarted = webcamContext?.videoStarted;
+  const navigate = useNavigate();
   const videoHeight = window.innerHeight;
   const videoWidth = window.innerWidth;
-  const aspectRatio = window.innerWidth / window.innerHeight;
   const displaySize = { width: videoWidth, height: videoHeight };
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     createCanvas()
     console.log('createdCanvas')
-  }, [canvasRef])
+  }, [])
 
+  useEffect(() => {
+    handleVideoOnPlay();
+  }, [faceMatcher])
 
   const createCanvas = () => {
-    if(videoRef.current){
-      canvasRef.current = faceapi.createCanvasFromMedia(videoRef.current);
+    if (videoStarted && webcamRef?.current?.video){
+      canvasRef.current = faceapi.createCanvasFromMedia(webcamRef.current.video);
     }
   };
 
-  const startVideo = async () => {
-    setCaptureVideo(true);
-    try{
-      const stream = await navigator.mediaDevices.getUserMedia({ video: { aspectRatio } })
-      if(videoRef.current){
-        videoRef.current.srcObject = stream;
-        videoRef.current.play();
-      }
-    } catch(err){
-      console.error("error:", err);
-    }
-  }
-
   const handleVideoOnPlay = () => {
-    if (canvasRef.current && videoRef.current) {
+    console.log('handling video play')
+    if (canvasRef.current && webcamRef?.current) {
       faceapi.matchDimensions(canvasRef.current, displaySize);
+      console.log('matching facial dimensions')
     }
     setInterval(async () => {
-      if (faceMatcher && videoRef.current && canvasRef.current && videoRef.current.readyState === videoRef.current.HAVE_ENOUGH_DATA) {
-        const detections = await faceapi.detectAllFaces(videoRef.current).withFaceLandmarks().withFaceDescriptors();
+      if (faceMatcher && webcamRef?.current?.video && canvasRef.current) {
+        console.log('1');
+        const detections = await faceapi
+          .detectAllFaces(webcamRef.current.video)
+          .withFaceLandmarks()
+          .withFaceDescriptors();
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
         const context = canvasRef.current.getContext('2d');
         if (context) {
@@ -66,39 +64,12 @@ const KillCam: React.FC<KillCamProps> = ({faceMatcher}) => {
     }, 100);
   };
 
-  const closeWebcam = () => {
-    if(videoRef.current && videoRef.current.srcObject){
-      const mediaStream = videoRef.current.srcObject as MediaStream;
-      videoRef.current.pause();
-      mediaStream.getTracks()[0].stop();
-      setCaptureVideo(false);
-    }
-  }
-
   return (
-    <div>
-      <div style={{ textAlign: 'center', padding: '10px' }}>
-        {
-          captureVideo ?
-            <button onClick={closeWebcam} style={{ cursor: 'pointer', backgroundColor: 'green', color: 'white', padding: '15px', fontSize: '25px', border: 'none', borderRadius: '10px' }}>
-              Close Webcam
-            </button>
-            :
-            <button onClick={startVideo} style={{ cursor: 'pointer', backgroundColor: 'green', color: 'white', padding: '15px', fontSize: '25px', border: 'none', borderRadius: '10px' }}>
-              Open Webcam
-            </button>
-        }
-      </div>
-      { captureVideo ?
-        <div>
-          <div style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
-            <video ref={videoRef} height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay} style={{ borderRadius: '10px' }} />
-            <canvas ref={canvasRef} style={{ position: 'absolute' }} />
-          </div>
-        </div>
-        : <div>loading...</div>
-      }
+    <div style={{ textAlign: 'center', padding: '10px', display: 'flex', justifyContent: 'center' }}>      
+      <button style={{ position: 'absolute', top:'10%' }} onClick={()=>{navigate('/gameover')}}> wow you won the game! </button>
+      <canvas ref={canvasRef} style={{ position: 'absolute' }} />
     </div>
   );
 }
+
 export default KillCam;
