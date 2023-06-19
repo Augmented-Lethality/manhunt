e8df635ebb7a7c0f805a63ff4a5290e6a21009c8
Alex <xanderlambert@gmail.com>
2023-06-19 16:19:14 -0500

    create basic trophy component

---
+++ src/components/Trophy.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+export const Trophy: React.FC = () => {
+  return (
+    <div>
+      <h1>You win this!</h1>
+      <h1></h1>
+      <h3>Thanks for playing.</h3>
+    </div>
+  );
+};
+
+
