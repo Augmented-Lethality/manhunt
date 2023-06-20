976224a453362554fc51fbaad4dde07a9bb3aead
Alex <xanderlambert@gmail.com>
2023-06-20 12:31:43 -0500

    add spin on click/drag of trophy

--- src/components/Trophy.tsx
+++ src/components/Trophy.tsx
@@ -1,28 +1,49 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from 'react-three-fiber';
+import React, { useRef, useState, useMemo } from 'react';
+import { Canvas } from 'react-three-fiber';
 import { Box } from '@react-three/drei';
 
-export const Trophy: React.FC = () => {
-  const cubeRef = useRef<THREE.Mesh>(null);
+interface TrophyProps {}
 
-  // useFrame(() => {
-  //   if (cubeRef.current) {
-  //     cubeRef.current.rotation.y += 0.01; // Adjust rotation speed
-  //   }
-  // });
+const Trophy: React.FC<TrophyProps> = () => {
+  const cubeRef = useRef<THREE.Mesh>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [prevMouseX, setPrevMouseX] = useState(0);
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsDragging(true);
+    setPrevMouseX(e.clientX);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!isDragging) return;
+
+    const mouseDeltaX = e.clientX - prevMouseX;
+    setPrevMouseX(e.clientX);
+
+    if (cubeRef.current) {
+      cubeRef.current.rotation.y += mouseDeltaX * 0.01;
+    }
+  };
+
 
   return (
-    <div>
+    <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
       <h1>You win this!</h1>
       <Canvas>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
-        <Box ref={cubeRef} args={[1, 1, 1]} position={[0, 0, 0]} rotation={[0, 0.4, 0]}>
-          <meshStandardMaterial attach="material" color="orange" />
+        <Box ref={cubeRef} args={[2, 2, 2]} position={[0, 0, 0]} rotation={[0, 0.4, 0]}>
+          <meshStandardMaterial attach="material" color={'orange'} />
         </Box>
       </Canvas>
       <h3>Thanks for playing.</h3>
     </div>
   );
 };
+
+export default Trophy;
 
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { ButtonToHome } from '../components/Buttons';
 import CreateFaceDescriptions from '../components/CreateFaceDescriptions';
-import { Trophy } from '../components/Trophy'
+import Trophy from '../components/Trophy';
 
 export type UserData = {
   username: string;
@@ -12,7 +12,7 @@ export type UserData = {
   gamesPlayed: number;
   gamesWon: number;
   killsConfirmed: number;
-  facialDescriptions: Array<number> | null
+  facialDescriptions: Array<number> | null;
   // Add other user data properties as needed
 };
 
@@ -41,55 +41,77 @@ const ProfilePage: React.FC<{ userData: UserData | null }> = () => {
     return null;
   }
 
-  if(isVerifying){
+  if (isVerifying) {
     return (
       <CreateFaceDescriptions
         setIsVerifying={setIsVerifying}
         username={user.name}
         userID={user.sub}
-        setUser={setUserData}/>
-    )
+        setUser={setUserData}
+      />
+    );
   }
 
   return (
-    <div className='content-layout' style={{ textAlign: 'center', fontWeight: 'bold',color: '#6e6b8c' }}>
-      <h1 id='page-title' className='content__title'>Profile</h1>
+    <div
+      className='content-layout'
+      style={{ textAlign: 'center', fontWeight: 'bold', color: '#6e6b8c' }}
+    >
+      <h1 id='page-title' className='content__title'>
+        Profile
+      </h1>
       <div className='content__body'>
         <div className='profile-grid'>
-          <div style={{display: 'flex', alignItems:'center'}}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <img
               src={user.picture}
               alt='Profile'
               className='profile__avatar'
-              style={{height: '14vh', width: '14vh'}}
+              style={{ height: '14vh', width: '14vh' }}
             />
-            <div style={{display:'flex', flexDirection:'column', margin: '2vh', alignItems: 'start'}}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: '2vh',
+                alignItems: 'start',
+              }}
+            >
               <h2 className='profile__title'>{user.name}</h2>
               <span className='profile__description'>
                 {user.email}
                 {/* <ButtonToUpdateEmail /> */}
               </span>
             </div>
-            
           </div>
           <Trophy />
           {userData?.facialDescriptions ? (
             <div className='profile_verification'>
-              <p style={{textAlign:'start', margin:'3vh'}}>
-                Citizen has been verified. The CorpoVerse thanks you for your cooperation.
+              <p style={{ textAlign: 'start', margin: '3vh' }}>
+                Citizen has been verified. The CorpoVerse thanks you for your
+                cooperation.
               </p>
-              <button onClick={()=>setIsVerifying(true)}>Feeling Patriotic? Reverify</button>
+              <button onClick={() => setIsVerifying(true)}>
+                Feeling Patriotic? Reverify
+              </button>
             </div>
           ) : (
             <div className='profile_verification'>
-              <h3>Citizen has not been processed by the CorpoReality Police.</h3>
+              <h3>
+                Citizen has not been processed by the CorpoReality Police.
+              </h3>
               <h3>Please send in Biodata to participate in SOCIETY™.</h3>
-              <button style={{background:'#6e6b8c', color: 'white'}} onClick={()=>setIsVerifying(true)}>Send BioData</button>
+              <button
+                style={{ background: '#6e6b8c', color: 'white' }}
+                onClick={() => setIsVerifying(true)}
+              >
+                Send BioData
+              </button>
             </div>
           )}
           <div className='profile__details'>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <h2>Games Played: {userData?.gamesPlayed}</h2>
             <h2>Games Won: {userData?.gamesWon}</h2>
             <h2>Kills Confirmed: {userData?.killsConfirmed}</h2>
