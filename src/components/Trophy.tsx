ed17709f9fe9a6f37eeac675ed1d40c6bd26e0ea
Alex <xanderlambert@gmail.com>
2023-06-21 15:14:17 -0500

    add torus to potential shapes, made it spin

--- src/components/Trophy.tsx
+++ src/components/Trophy.tsx
@@ -1,11 +1,13 @@
 import React, { useRef, useState, useMemo } from 'react';
 import { Canvas } from 'react-three-fiber';
-import { Box, Sphere, Cylinder } from '@react-three/drei';
+import { Box, Dodecahedron, Torus } from '@react-three/drei';
 
 interface TrophyProps {}
 
 const Trophy: React.FC<TrophyProps> = () => {
-  const shapeRef = useRef<THREE.Mesh>(null);
+  const cubeRef = useRef<THREE.Mesh>(null);
+  const dodecahedronRef = useRef<THREE.Mesh>(null);
+  const torusRef = useRef<THREE.Mesh>(null);
   const [isDragging, setIsDragging] = useState(false);
   const [prevMouseX, setPrevMouseX] = useState(0);
 
@@ -24,8 +26,16 @@ const Trophy: React.FC<TrophyProps> = () => {
     const mouseDeltaX = e.clientX - prevMouseX;
     setPrevMouseX(e.clientX);
 
-    if (shapeRef.current) {
-      shapeRef.current.rotation.y += mouseDeltaX * 0.01;
+    if (cubeRef.current) {
+      cubeRef.current.rotation.y += mouseDeltaX * 0.01;
+    }
+
+    if (dodecahedronRef.current) {
+      dodecahedronRef.current.rotation.y += mouseDeltaX * 0.01;
+    }
+
+    if (torusRef.current) {
+      torusRef.current.rotation.y += mouseDeltaX * 0.01;
     }
   };
 
@@ -34,15 +44,15 @@ const Trophy: React.FC<TrophyProps> = () => {
     const randomIndex = Math.floor(Math.random() * dimensions.length);
     return dimensions[randomIndex];
   };
-
+  
   const getRandomColor = (): string => {
-    const colors = ['red', 'lightGreen', 'blue', 'yellow', 'orange', 'purple', 'pink', 'gray'];
+    const colors = ['darkred', 'lightgreen', 'blue', 'yellow', 'orange', 'purple', 'pink'];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
-  };
+  }  
 
-  const getRandomShape = (): React.ReactElement => {
-    const shapes = [<Box />, <Sphere />, <Cylinder />];
+  const getRandomShape = (): string => {
+    const shapes = ['box', 'polyhedron', 'torus'];
     const randomIndex = Math.floor(Math.random() * shapes.length);
     return shapes[randomIndex];
   };
@@ -51,19 +61,39 @@ const Trophy: React.FC<TrophyProps> = () => {
   const dimension = useMemo(() => getRandomDimension(), []);
   const color = useMemo(() => getRandomColor(), []);
 
+  const trophyObj = {
+    shape: shape,
+    dimension: dimension,
+    color,
+  };
+
   return (
     <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
       <h1>You win this!</h1>
       <Canvas>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
-        {React.cloneElement(shape, {
-          ref: shapeRef,
-          args: [dimension, dimension, dimension],
-          position: [0, 0, 0],
-          rotation: [0, 0.4, 0],
-          children: <meshStandardMaterial attach="material" color={color} />,
-        })}
+        {shape === 'box' && (
+          <Box ref={cubeRef} args={[dimension, dimension, dimension]} position={[0, 0, 0]} rotation={[0, 0.4, 0]}>
+            <meshStandardMaterial attach="material" color={color} />
+          </Box>
+        )}
+        {shape === 'polyhedron' && (
+          <Dodecahedron ref={dodecahedronRef} args={[dimension, 0]} position={[0, 0, 0]} rotation={[0, 0.4, 0]}>
+            <meshStandardMaterial attach="material" color={color} />
+          </Dodecahedron>
+        )}
+        {shape === 'torus' && (
+          <Torus
+            ref={torusRef}
+            args={[dimension, 0.2, 16, 100]} // Adjust the arguments according to your desired torus shape
+            position={[0, 0, 0]}
+            rotation={[0, 0.4, 0]}
+          >
+            <meshStandardMaterial attach="material" color={color} />
+          </Torus>
+        )}
+        
       </Canvas>
       <h3>Thanks for playing.</h3>
     </div>
