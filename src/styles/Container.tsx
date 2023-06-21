4e9d4d30bfa735e46f8f3450519db8eea2f4458b
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-21 15:31:54 -0500

    Init

---
+++ src/styles/Container.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  max-width: 400px;
+  margin: 0;
+  color: #6e6b8c;
+`;
