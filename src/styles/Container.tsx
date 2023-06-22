c0002013ee57cd2e4e57fdac9347562c616eeebf
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-21 20:47:03 -0500

    Change height and width

--- src/styles/Container.tsx
+++ src/styles/Container.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
+  height: 100vh;
+  width: 100%;
   max-width: 400px;
   margin: 0;
   color: #6e6b8c;
