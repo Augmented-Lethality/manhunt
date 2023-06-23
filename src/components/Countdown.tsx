7bbc2ae7a12073ed84bb0598b169f10d22e321a0
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-23 18:57:52 -0500

    Add countdown stylings

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 interface CountdownProps {
   initialCount: number;
+  id: string;
 }
 
-const Countdown: React.FC<CountdownProps> = ({ initialCount }) => {
+const CountdownContainer = styled.div`
+    position: absolute;
+    color: #ffffffa1;
+    font-size: 2.5rem;
+    top: 4.2vh;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+const Countdown: React.FC<CountdownProps> = ({ initialCount, id }) => {
   const [minutes, setMinutes] = useState(Math.floor(initialCount / 60));
   const [seconds, setSeconds] = useState(initialCount % 60);
 
@@ -24,9 +35,9 @@ const Countdown: React.FC<CountdownProps> = ({ initialCount }) => {
   }, [minutes, seconds]);
 
   return (
-    <div>
+    <CountdownContainer id={id}>
       {`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}
-    </div>
+    </CountdownContainer>
   );
 };
 
