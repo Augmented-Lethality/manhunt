e8e91664e0614947ff38e7acdb54dd1f5560013c
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-23 18:58:09 -0500

    Add stylings and working links

--- src/components/DropDownMenu.tsx
+++ src/components/DropDownMenu.tsx
@@ -2,25 +2,43 @@ import React, { FC, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { BsThreeDots } from 'react-icons/bs/';
 import { IoIosCloseCircle } from 'react-icons/io/';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+import { FaUserFriends } from 'react-icons/fa';
+import { FiLogOut, FiSettings } from 'react-icons/fi';
 
 const dropdownAnimation = keyframes`
-  0% { opacity: 0; transform: translateY(-10px); }
+  0% { opacity: 0; transform: translateY(-10vh); }
   100% { opacity: 1; transform: translateY(0); }
+`;
+
+const Backdrop = styled.div<{ isOpen: boolean }>`
+  display: ${props => props.isOpen ? 'block' : 'none'};
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 1;
 `;
 
 
 const Menu = styled.div<{ isOpen: boolean }>`
   display: ${props => props.isOpen ? 'block' : 'none'};
-  animation: ${dropdownAnimation} 0.5s ease-in-out;
+  animation: ${dropdownAnimation} 0.4s ease-in-out;
   position: absolute;
-  background: #2a2a32;
-  border: 1px solid #202026;
-  padding: 1rem;
+  background: #242429;
+  border-bottom: 1px solid #070707;
+  padding-top: 1rem;
+  top: 0;
   right: 0;
-  top: 8.5vh;
+  width: 100%;
+  z-index: 2;
   & > * {
     display: flex;
-    align-items: center;
+    align-items: end;
+    margin: 1.5rem;
   }
   & > * > * {
     margin-right: 10px;
@@ -34,6 +52,8 @@ interface DropDownMenuProps {
 
 const DropDownMenu: FC<DropDownMenuProps> = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const { logout } = useAuth0();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -52,21 +72,24 @@ const DropDownMenu: FC<DropDownMenuProps> = ({ children }) => {
 const Close = styled(IoIosCloseCircle)`
 height: 10vw;
 width: 10vw;
-margin-left: 10px;
-border-radius: 50%;
-border: 2px solid #5E5E63;
 padding: 0;
-box-sizing: border-box;
+z-index: 3;
+position: absolute;
+top: 0;
+right: 0;
 `;
   return (
     <>
-      {!isMenuOpen ?
       <Dots onClick={toggleMenu} />
-      : <Close onClick={toggleMenu} />
-      }
+      <Backdrop isOpen={isMenuOpen} onClick={toggleMenu}/>
       <Menu isOpen={isMenuOpen}>
+        <Close onClick={toggleMenu} />
         {children}
+        <div onClick={()=>{navigate('/friends')}}><FaUserFriends/>friends</div>
+        <div onClick={()=>{navigate('/settings')}}><FiSettings/>settings</div>
+        <div onClick={()=>{logout({logoutParams: {returnTo: window.location.origin}})}}><FiLogOut/>logout</div>
       </Menu>
+
     </>
   );
 };
