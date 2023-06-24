d980ec63309c0818fdf9bccae11762dca6c48438
logan <loganh42@gmail.com>
2023-06-24 11:18:01 -0500

    Render Pick Victim Button for Host Only

--- src/components/WhosHunting.tsx
+++ src/components/WhosHunting.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import SocketContext, { User } from '../contexts/Socket/SocketContext';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const pickVictim = (users: User[], SetHunted: (user: User) => void) => {
   const victim = users[Math.floor(Math.random() * users.length)];
@@ -7,24 +8,30 @@ const pickVictim = (users: User[], SetHunted: (user: User) => void) => {
 };
 
 const WhosHunting: React.FunctionComponent = () => {
+  const { user } = useAuth0();
+
   const { SetHunted } = useContext(SocketContext);
   const { users, games } = useContext(SocketContext).SocketState;
 
-  // useEffect(() => {
-  //   if (games.length > 0 && games[0].hunted.length === 0) {
-  //     pickVictim(users, SetHunted);
-  //   }
-  // }, [games, SetHunted, users]);
+  useEffect(() => {
+
+  }, [games, SetHunted, users]);
 
   return (
     <div>
       {games.length > 0 && games[0].hunted.length > 0 ? (
         <div>
           <div>Player {games[0].hunted}, You're Being Hunted</div>
-          <button onClick={() => pickVictim(users, SetHunted)}>Pick Again</button>
+          {user?.sub === games[0].host ? <button onClick={() => pickVictim(users, SetHunted)}>Pick Again</button>
+            : <></>
+          }
         </div>
       ) : (
-        <button onClick={() => pickVictim(users, SetHunted)}>Who's Being Hunted?</button>
+        <>
+          {user?.sub === games[0].host ? <button onClick={() => pickVictim(users, SetHunted)}>Who's Being Hunted?</button>
+            : <div>Victim Has Not Been Selected</div>
+          }
+        </>
       )}
     </div>
   );
