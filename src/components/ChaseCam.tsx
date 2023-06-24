0516e62f89d445cb0262d78ae7c9a7369b70d98f
logan <loganh42@gmail.com>
2023-06-24 14:28:26 -0500

    Fixed Camera Turning Off on Leave Game Page

--- src/components/ChaseCam.tsx
+++ src/components/ChaseCam.tsx
@@ -199,6 +199,7 @@ const ChaseCam = forwardRef<ChaseCamRefType, ChaseCamProps>((props, ref) => {
       window.removeEventListener('mouseup', handleMouseUp);
       window.removeEventListener('mousemove', handleMouseMove);
 
+      turnOffCamera();
     };
   }, []);
   /////// /////////////////////////////////// //////
