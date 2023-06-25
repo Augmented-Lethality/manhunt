ba4c6890609794d95342da618f4de9cba42cb5e5
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-25 12:21:48 -0500

    Add server requests

--- src/pages/FriendsPage.tsx
+++ src/pages/FriendsPage.tsx
@@ -6,6 +6,8 @@ import Loading from '../components/Loading';
 import {Container} from '../styles/Container';
 import {Main} from '../styles/Main';
 import {Header} from '../styles/Header';
+import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const FriendsContainer = styled.div`
   background-color: #3C3E49;
@@ -41,10 +43,65 @@ const FriendsPage: React.FC = () => {
   const [onlineFriends, setOnlineFriends] = useState([]);
   const [offlineFriends, setOfflineFriends] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
+  const { user, isAuthenticated } = useAuth0();
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    if(user && isAuthenticated) {
+      getFriends();
+    }
+  }, [user, isAuthenticated]); 
+
+  useEffect(() => {
+    handleSearch();
+  }, [searchText]); 
+
+  const getFriends = async () => {
+    try {
+      const res = await axios.get('/friends', {
+        params: {
+          userId: user?.authId,
+          status: 'accepted'
+        }
+     });
+      if (res.status === 200) {
+        let onlineFriends;
+        let offlineFriends;
+        res.data.forEach(friend => {
+          if(friend.gameId){
+            onlineFriends.push(friend);
+          } else {
+            offlineFriends.push(friend);
+          }
+        });
+        setOnlineFriends(onlineFriends);
+        setOfflineFriends(offlineFriends);
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  const handleSearch = async () => {
+    if(!searchText.length){
+      return;
+    }
+    try {
+      const res = await axios.get(`/users/search/${searchText}`)
+      if(res.status === 200){
+        setSearchResults(res.data)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
   };
+
+  if(!user || !isAuthenticated){
+    return null;
+  }
 
   if(!onlineFriends || !offlineFriends){
     return (
@@ -68,7 +125,7 @@ const FriendsPage: React.FC = () => {
             type='text'
             placeholder="Search"
             value={searchText}
-            onChange={handleSearchChange}
+            onChange={handleInputChange}
             />
         {searchText === '' ? <SearchIcon className='react-icon'/> : <CloseIcon className='react-icon'/>}
        </SearchBar>
