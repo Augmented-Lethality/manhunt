55f2c14aae691cee63b6cac6f6652837daab717e
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-25 10:31:15 -0500

    Import Containers, add styings to search and containers

--- src/pages/FriendsPage.tsx
+++ src/pages/FriendsPage.tsx
@@ -3,20 +3,30 @@ import styled from 'styled-components';
 import UsersList from '../components/UsersList';
 import { IoSearchCircle, IoCloseCircle } from 'react-icons/io5';
 import Loading from '../components/Loading';
+import {Container} from '../styles/Container';
+import {Main} from '../styles/Main';
+import {Header} from '../styles/Header';
 
-const Container = styled.div`
-  background-color: #333;
-  padding: 10px;
-  margin: 10px;
+const FriendsContainer = styled.div`
+  background-color: #3C3E49;
+  padding: 20px;
+  margin: 20px;
+  margin-bottom: 0;
+  flex-grow: 1;
 `;
 
-const SearchInput = styled.input`
-  background-color: #222;
+const SearchBar = styled.div`
+  display: flex;
+  align-items: center;
+  margin-inline: auto;
+  background-color: #2B2B32;
   color: #fff;
   padding: 10px;
   border: none;
-  width: 100%;
+  width: 70%;
+  height: 30px;
 `;
+
 
 const SearchIcon = styled(IoSearchCircle)`
   color: #fff;
@@ -26,37 +36,57 @@ const CloseIcon = styled(IoCloseCircle)`
   color: #fff;
 `;
 
-interface FriendsPageProps {
-  onlineFriends: Array<{ image: string; username: string }>;
-  offlineFriends: Array<{ image: string; username: string }>;
-  // Adjust the type of searchUsers based on your implementation
-  searchUsers: Array<{ image: string; username: string }>;
-}
-
-const FriendsPage: React.FC<FriendsPageProps> = ({ onlineFriends, offlineFriends, searchUsers }) => {
+const FriendsPage: React.FC = () => {
   const [searchText, setSearchText] = useState('');
+  const [onlineFriends, setOnlineFriends] = useState([]);
+  const [offlineFriends, setOfflineFriends] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
   };
 
+  if(!onlineFriends || !offlineFriends){
+    return (
+      <Container>
+        <Header page='Friends'/>
+        <Main style={{marginBottom: 0}}>
+          <FriendsContainer>
+            <Loading/>
+          </FriendsContainer>
+        </Main>
+      </Container>
+    )
+  }
+
   return (
     <Container>
-      <SearchInput placeholder="Search" value={searchText} onChange={handleSearchChange} />
-      {searchText === '' ? <CloseIcon /> : <SearchIcon />}
-
-      {searchText === '' ? (
-        <>
-          <UsersList users={onlineFriends} header={`Online • ${onlineFriends.length}`} />
-          <UsersList users={offlineFriends} header={`Offline • ${offlineFriends.length}`} />
-        </>
-      ) : searchUsers.length === 0 ? (
-        <Loading />
-      ) : (
-        <UsersList users={searchUsers} />
-      )}
+      <Header page='Friends'/>
+      <Main style={{marginBottom: 0, display:'flex'}}>
+        <SearchBar>
+          <input
+            type='text'
+            placeholder="Search"
+            value={searchText}
+            onChange={handleSearchChange}
+            />
+        {searchText === '' ? <SearchIcon className='react-icon'/> : <CloseIcon className='react-icon'/>}
+       </SearchBar>
+        <FriendsContainer>
+          {searchText === '' ? (
+            <>
+              <UsersList users={onlineFriends} header={`Online • ${onlineFriends.length}`} />
+              <UsersList users={offlineFriends} header={`Offline • ${offlineFriends.length}`} />
+            </>
+          ) : searchResults.length === 0 ? (
+            <Loading />
+          ) : (
+            <UsersList users={searchResults} />
+          )}
+        </FriendsContainer>
+      </Main>
     </Container>
-  );
+  )
 };
 
 export default FriendsPage;
