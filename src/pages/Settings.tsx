66a26b00a7d6eb3941e7625ff864071fc63eb865
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-26 05:59:42 -0500

    Revert bakc to old slider

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,79 +1,82 @@
-import React, {useEffect} from 'react';
-import styled from 'styled-components';
-import { useFontSize } from '../contexts/FontSize';
+import React from 'react'
 import { Header } from '../styles/Header';
 import { Container } from '../styles/Container';
 import { Main } from '../styles/Main';
-import Switch from 'react-switch';
+import styled from 'styled-components';
+import { useFontSize } from '../contexts/FontSize';
 
-// const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
-//   height: 0;
-//   width: 0;
-//   visibility: hidden;
-// `;
+const SwitchInput = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+`;
 
-// const Slider = styled.span`
-//   content: '';
-//   position: absolute;
-//   top: 4px;
-//   left: 4px;
-//   width: 26px;
-//   height: 26px;
-//   background: #fff;
-//   border-radius: 50%;
-//   transition: 0.4s;
-// `;
+const Slider = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #2a2937;
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: 34px;
+  &:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: #dddddd;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 50%;
+  }
+`;
 
-// const SwitchLabel = styled.label<{ checked: boolean }>`
-//   cursor: pointer;
-//   width: 60px;
-//   height: 34px;
-//   background: ${props => props.checked ? '#7A9EC2' : '#2a2937'};
-//   display: block;
-//   border-radius: 34px;
-//   position: relative;
+const SwitchLabel = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+  margin: 30px;
+  & ${SwitchInput}:checked + ${Slider} {
+    background-color: #7A9EC2;
+    &:before {
+      -webkit-transform: translateX(26px);
+      -ms-transform: translateX(26px);
+      transform: translateX(26px);
+    }
+  }
 
-//   ${HiddenCheckbox}:checked + & {
-//     background: #7A9EC2;
-//   }
+`;
 
-//   ${HiddenCheckbox}:checked + & ${Slider} {
-//     left: calc(100% - 4px);
-//     transform: translateX(-100%);
-//   }
-// `;
+const Switch = ({ ...props }) => (
+  <SwitchLabel>
+    <SwitchInput type="checkbox" {...props} />
+    <Slider className="slider round"></Slider>
+  </SwitchLabel>
+);
 
-// interface SwitchProps {
-//   onChange: () => void;
-//   checked: boolean;
-// }
-
-// const Switch: React.FC<SwitchProps> = ({ onChange, checked }) => (
-//   <>
-//     <HiddenCheckbox id="switch" onChange={onChange} checked={checked} />
-//     <SwitchLabel checked={checked} htmlFor="switch">
-//       <Slider />
-//     </SwitchLabel>
-//   </>
-// );
 
 const Settings: React.FC = () => {
   const [fontSize, setFontSize] = useFontSize();
-  const [checked, setChecked] = React.useState(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-    setTimeout(() => setFontSize(event.target.checked ? 20 : 16), 500);
+  const toggleLargeFont = () => {
+    if(fontSize === 16) {
+      setFontSize(20);
+    } else {
+      setFontSize(16);
+    }
   }
 
   return (
     <Container>
       <Header page='Settings'/>
-      <Main style={{flexDirection:'row', alignItems:'start'}}>
-        <label>
-          <input type="checkbox" style={{height:'40px', width:'40px', margin:'20px'}} onChange={handleChange} checked={checked} />
-          <h3>larger font</h3>
-        </label>
+      <Main style={{flexDirection:'row', alignItems:'baseline'}}>
+        <Switch onChange={toggleLargeFont}/>
+        <h3>Larger Font</h3>
       </Main>
     </Container>
   )
