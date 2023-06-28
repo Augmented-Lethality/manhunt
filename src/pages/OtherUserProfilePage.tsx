7c9d8035b3dda571a47ea04d6874bf1280353be2
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-28 18:17:41 -0500

    Uncomment some things

--- src/pages/OtherUserProfilePage.tsx
+++ src/pages/OtherUserProfilePage.tsx
@@ -1,10 +1,10 @@
-// import { useAuth0 } from '@auth0/auth0-react';
-// import React, { useEffect, useState } from 'react';
+//import { useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect, useState } from 'react';
 // import { useParams } from 'react-router-dom';
 // import axios from 'axios';
-// import { Container } from '../styles/Container';
-// import { Header } from '../styles/Header';
-// import { Main } from '../styles/Main';
+import { Container } from '../styles/Container';
+import { Header } from '../styles/Header';
+import { Main } from '../styles/Main';
 // export type UserData = {
 //   username: string;
 //   email: string;
@@ -16,65 +16,65 @@
 //   // Add other user data properties as needed
 // };
 
-//  const OtherUserProfilePage: React.FC = () => {
-//   // const { user, isAuthenticated } = useAuth0();
-//   // const { OtherUserUsername } = useParams();
-//    const [OtherUserData, setOtherUserData] = useState<UserData | null>(null);
+ const OtherUserProfilePage: React.FC = () => {
+  // const { user, isAuthenticated } = useAuth0();
+  // const { username } = useParams();
+  //  const [OtherUserData, setOtherUserData] = useState<UserData | null>(null);
 
-//   // useEffect(() => {
-//   //     fetchUserData();
-//   // }, []);
+  // useEffect(() => {
+  //     fetchUserData();
+  // }, []);
 
-//   // const fetchUserData = async () => {
-//   //   try {
-//   //     const res = await axios.get<UserData>(`/Users/${OtherUserUsername}`)
+  // const fetchUserData = async () => {
+  //   try {
+  //     const res = await axios.get<UserData>(`/Users/${OtherUserUsername}`)
       
-//   //     setOtherUserData(res.data);
-//   //   } catch (error) {
-//   //     console.error('Error fetching user data:', error);
-//   //   }
-//   // };
+  //     setOtherUserData(res.data);
+  //   } catch (error) {
+  //     console.error('Error fetching user data:', error);
+  //   }
+  // };
 
-//   // if (!isAuthenticated) {
-//   //   return null;
-//   // }
+  // if (!isAuthenticated) {
+  //   return null;
+  // }
 
-//    return (
-//      <Container>
-//   //     <Header page='Profile' />
-//   //     <Main>
-//   //       <div className='content__body'>
-//   //         <div className='profile-grid'>
-//   //           <div style={{ display: 'flex', alignItems: 'center' }}>
-//   //             {user.picter ? (
-//   //               <img
-//   //                 src={user.picture}
-//   //                 className='profile__avatar'
-//   //                 style={{ height: '14vh', width: '14vh', borderRadius:'50%'}}
-//   //               />
-//   //             ) : (
-//   //               <h1 className='alt-user-pic-large'>
-//   //                 {user.name?.slice(0, 1)}
-//   //               </h1>
-//   //             )}
-//   //             <div style={{ display: 'flex', flexDirection: 'column', margin: '2vh', alignItems: 'start' }}>
-//   //               <h2 className='profile__title'>{user.name}</h2>
-//   //               <span className='profile__description'>
-//   //               </span>
-//   //             </div>
-//   //           </div>
-//   //           <div className='profile__details'>
-//   //             <br />
-//   //             <br />
-//   //             <h2>Games Played: {OtherUserData?.gamesPlayed}</h2>
-//   //             <h2>Games Won: {OtherUserData?.gamesWon}</h2>
-//   //             <h2>Kills Confirmed: {OtherUserData?.killsConfirmed}</h2>
-//   //           </div>
-//   //         </div>
-//   //       </div>
-//   //     </Main>
-//      </Container>
-//    );
-//  };
+   return (
+     <Container>
+  //     <Header page='Profile' />
+  //     <Main>
+  {/* //       <div className='content__body'>
+  //         <div className='profile-grid'>
+  //           <div style={{ display: 'flex', alignItems: 'center' }}>
+  //             {user.picter ? (
+                <img
+                  src={user.picture}
+                  className='profile__avatar'
+                  style={{ height: '14vh', width: '14vh', borderRadius:'50%'}}
+                />
+              ) : (
+                <h1 className='alt-user-pic-large'>
+                  {user.name?.slice(0, 1)}
+                </h1>
+              )}
+              <div style={{ display: 'flex', flexDirection: 'column', margin: '2vh', alignItems: 'start' }}>
+                <h2 className='profile__title'>{user.name}</h2>
+                <span className='profile__description'>
+                </span>
+              </div>
+            </div>
+            <div className='profile__details'>
+              <br />
+              <br />
+              <h2>Games Played: {OtherUserData?.gamesPlayed}</h2>
+              <h2>Games Won: {OtherUserData?.gamesWon}</h2>
+              <h2>Kills Confirmed: {OtherUserData?.killsConfirmed}</h2>
+            </div>
+          </div>
+        </div> */}
+      </Main>
+     </Container>
+   );
+ };
 
-// export default OtherUserProfilePage;
+export default OtherUserProfilePage;
