2329d30ad977d896adc7957e5e20c490547ab6ae
logan <loganh42@gmail.com>
2023-07-01 00:18:39 -0500

    Create Waiting Gif

---
+++ src/components/WaitSubmitPhoto.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WaitSubmitPhoto: React.FC = () => {
+  return (
+    <div>
+      <img src='https://i.imgur.com/KnyWrG4.gif' alt="GIF" />
+    </div>
+  );
+};
+
+export default WaitSubmitPhoto;
