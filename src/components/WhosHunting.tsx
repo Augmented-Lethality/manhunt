741ce4da4c5b1c3738e5a649efd23ae1f0960725
Kalypso Homan <catcatmcgee@gmail.com>
2023-07-02 17:17:49 -0500

    Simplify and add conditional rendering for views

--- src/components/WhosHunting.tsx
+++ src/components/WhosHunting.tsx
@@ -1,53 +1,39 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import SocketContext, { User } from '../contexts/Socket/SocketContext';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const pickVictim = (users: User[], SetHunted: (user: User) => void) => {
-  const victim = users[Math.floor(Math.random() * users.length)];
-  SetHunted(victim);
-};
+interface WhosHuntingProps {
+  setBountyName: React.Dispatch<React.SetStateAction<string | null>>
+}
 
-const WhosHunting: React.FunctionComponent = () => {
+const WhosHunting: React.FunctionComponent<WhosHuntingProps> = ({setBountyName}) => {
   const { user } = useAuth0();
   const { SetHunted } = useContext(SocketContext);
   const { users, games } = useContext(SocketContext).SocketState;
+  const game = games[0];
 
-  const [huntedName, setHuntedName] = useState('');
-
-  useEffect(() => {
-    // console.log(games);
-    if (games[0].hunted.length > 0 && users) {
-      const matchingUser = users.filter(user => user.authId === games[0].hunted);
-      setHuntedName(matchingUser[0].username)
-    }
-  }, [games, SetHunted, users]);
+  const pickVictim = (users: User[], SetHunted: (user: User) => void) => {
+    //Chose a random victim from the players
+    const bounty = users[Math.floor(Math.random() * users.length)];
+    //Set the socket context to include the bounty
+    SetHunted(bounty);
+    //Grab the bounty's username to display to the players
+    const matchingUser = users.filter(user => user.authId === games[0].hunted);
+    setBountyName(matchingUser[0].username)
+  };
 
   if (!games || games.length === 0) {
     return <div>Loading Lobby</div>;
   }
 
-  const game = games[0];
-
-  if (!game.hunted || game.hunted.length === 0) {
-    if (user?.sub === game.host) {
-      return (
-        <div>
-          <button onClick={() => pickVictim(users, SetHunted)}>Who's Being Hunted?</button>
-        </div>
-      );
-    } else {
-      return <div>Victim Has Not Been Selected</div>;
-    }
-  }
-
   return (
-    <div>
-      <div>{huntedName}, you're being hunted.</div>
-      {user?.sub === game.host && (
-        <button onClick={() => pickVictim(users, SetHunted)}>Pick Again</button>
-      )}
-    </div>
-  );
+    user?.sub === game?.host
+    ? <button
+        onClick={() => pickVictim(users, SetHunted)}
+        style={{maxWidth:'200px'}}>Start
+      </button>
+    : <button style={{flexGrow: 2}}>Waiting on Host</button>
+  )
 };
 
 export default WhosHunting;
