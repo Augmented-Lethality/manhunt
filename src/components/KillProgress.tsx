6f3ed67c249811ed2ba77f97098058091df4f57b
Kalypso Homan <catcatmcgee@gmail.com>
2023-07-13 20:09:52 -0500

    Remove extra indent

--- src/components/KillProgress.tsx
+++ src/components/KillProgress.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledSvg = styled.svg`
-    position: absolute;
-    transform: rotate(-90deg);
+  position: absolute;
+  transform: rotate(-90deg);
 `;
 
 const ProgressCircle = styled.circle<{ progress: number, circumference: number }>`
-    stroke: #f06;
-    fill: transparent;
-    stroke-width: 3;
-    stroke-dasharray: ${(props) => props.circumference};
-    stroke-dashoffset: ${(props) => props.circumference};
-    transition: stroke-dashoffset 0.5s ease-out;
-    stroke-dashoffset: ${(props) => props.circumference - props.progress / 10 * props.circumference};
+  stroke: #f06;
+  fill: transparent;
+  stroke-width: 3;
+  stroke-dasharray: ${(props) => props.circumference};
+  stroke-dashoffset: ${(props) => props.circumference};
+  transition: stroke-dashoffset 0.5s ease-out;
+  stroke-dashoffset: ${(props) => props.circumference - props.progress / 10 * props.circumference};
 `;
 
 interface TargetRecognitionProps {
@@ -21,33 +21,33 @@ interface TargetRecognitionProps {
 }
 
 const TargetRecognition: React.FC<TargetRecognitionProps> = ({ progress }) => {
-    const radius = 20;
-    const stroke = 3;
-    const normalizedRadius = radius - stroke * 2;
-    const circumference = normalizedRadius * 2 * Math.PI;
+  const radius = 20;
+  const stroke = 3;
+  const normalizedRadius = radius - stroke * 2;
+  const circumference = normalizedRadius * 2 * Math.PI;
 
-    return (
-        <StyledSvg
-            height={radius * 2}
-            width={radius * 2}
-        >
-            <circle
-                stroke="#6cf"
-                fill="transparent"
-                strokeWidth={stroke}
-                r={normalizedRadius}
-                cx={radius}
-                cy={radius}
-            />
-            <ProgressCircle
-                progress={progress}
-                circumference={circumference}
-                r={normalizedRadius}
-                cx={radius}
-                cy={radius}
-            />
-        </StyledSvg>
-    );
+  return (
+    <StyledSvg
+      height={radius * 2}
+      width={radius * 2}
+    >
+      <circle
+        stroke="#6cf"
+        fill="transparent"
+        strokeWidth={stroke}
+        r={normalizedRadius}
+        cx={radius}
+        cy={radius}
+      />
+      <ProgressCircle
+        progress={progress}
+        circumference={circumference}
+        r={normalizedRadius}
+        cx={radius}
+        cy={radius}
+      />
+    </StyledSvg>
+  );
 };
 
 export default TargetRecognition;
