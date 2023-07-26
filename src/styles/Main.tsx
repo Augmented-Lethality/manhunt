25e5a77cd8ad7df9b783d443d9c058120955af45
Kalypso Homan <catcatmcgee@gmail.com>
2023-07-25 21:08:47 -0500

    Make a GameMain with a black background

--- src/styles/Main.tsx
+++ src/styles/Main.tsx
@@ -10,3 +10,14 @@ export const Main = styled.main`
   min-height: 100vh;
   width: 100vw;
 `;
+
+export const GameMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow: auto;
+  position: relative;
+  min-height: 100vh;
+  width: 100vw;
+  background: #1a1a1a;
+`;
