71abe3f2ca00325f6b2fc26e09bd01cd4df04ad3
Kalypso Homan <catcatmcgee@gmail.com>
2023-07-26 00:28:31 -0500

    Add height to GameMain

--- src/styles/Main.tsx
+++ src/styles/Main.tsx
@@ -15,8 +15,8 @@ export const GameMain = styled.main`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-  overflow: auto;
   position: relative;
+  height: 100%;
   min-height: 100vh;
   width: 100vw;
   background: #1a1a1a;
